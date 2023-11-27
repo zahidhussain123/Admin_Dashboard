9e1f5688dccbdb679af89f876ef79fb61d9902ef
Zahid Hussain <zahid4307@gmail.com>
2023-11-27 23:21:45 +0500

    line charts added

---
+++ src/components/chartBox/ChartBox.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import "./chartBox.scss";
+import { Link } from "react-router-dom";
+import { ResponsiveContainer, LineChart, Line, Tooltip } from "recharts";
+
+type Props =  {
+    color: string;
+    icon: string;
+    title: string;
+    dataKey: string;
+    number: number | string;
+    percentage: number;
+    chartData : object[]
+}
+
+const ChartBox = (props:Props) => {
+
+  return (
+    <div className="chartBox">
+      <div className="boxInfo">
+        <div className="title">
+          <img src={props.icon} alt="user" />
+                  <span>{ props.title}</span>
+        </div>
+              <h1>{ props.number}</h1>
+        <Link to="/" style={{color : props.color}}>View all</Link>
+      </div>
+      <div className="chartInfo">
+        <div className="chart">
+          <ResponsiveContainer width="99%" height="100%">
+            <LineChart data={props.chartData}>
+              <Tooltip
+                contentStyle={{ background: "transparent", border: "none" }}
+                labelStyle={{ display: "none" }}
+                position={{ x: 10, y: 60 }}
+              />
+              <Line
+                type="monotone"
+                stroke={props.color}
+                strokeWidth={2}
+                dataKey={props.dataKey}
+                dot={false}
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
+        <div className="texts">
+          <span className="percentage" style={{color : props.percentage < 0 ? "tomato" : "limegreen"}}>45%</span>
+          <span className="duration">this month</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChartBox;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import "./home.scss"
+import Topbar from '../../components/topbar/Topbar'
+import ChartBox from '../../components/chartBox/ChartBox'
+import { chartBoxConversion, chartBoxProduct, chartBoxRevenue, chartBoxUser } from '../../data'
 
 const Home = () => {
   return (
-    <div className='home'>Home</div>
+    <div className='home'>
+      <div className="box box1">
+        <Topbar />
+      </div> 
+      <div className="box box2">
+       <ChartBox {...chartBoxUser} />       
+      </div> 
+      <div className="box box3">
+       <ChartBox {...chartBoxProduct} />
+      </div> 
+      <div className="box box4">
+      </div> 
+        
+      <div className="box box5">
+      <ChartBox {...chartBoxConversion} />
+      </div> 
+      <div className="box box6">
+      <ChartBox {...chartBoxRevenue} />
+      </div> 
+     <div className="box box7"></div> 
+     <div className="box box8"></div> 
+     <div className="box box9"></div> 
+    </div>
   )
 }
 
