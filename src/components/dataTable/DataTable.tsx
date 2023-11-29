66291a61f945d903641bf53297fda91b508f0d21
Zahid Hussain <zahid4307@gmail.com>
2023-11-29 17:29:18 +0500

    users and products done with the generic type modal

---
+++ src/components/dataTable/DataTable.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import "./dataTable.scss";
+import { DataGrid, GridColDef, GridToolbar } from "@mui/x-data-grid";
+import { Link } from "react-router-dom";
+
+type Props = {
+  columns: GridColDef[];
+  rows: object[];
+  slug: string;
+};
+
+const DataTable = (props: Props) => {
+  const handleDelete = (id: number) => {};
+  const actionColumn: GridColDef = {
+    field: "action",
+    headerName: "Action",
+    width: 200,
+    renderCell: (params) => {
+      return (
+        <div className="action">
+          <Link to={`${props.slug}/${params.row.id}`}>
+            <img src="/view.svg" alt="edit" />
+          </Link>
+          <div className="delete" onClick={() => handleDelete(params.row.id)}>
+            <img src="/delete.svg" alt="delete" />
+          </div>
+        </div>
+      );
+    },
+  };
+
+  return (
+    <div className="dataTable">
+      <DataGrid
+        className="dataGrid"
+        rows={props.rows}
+        columns={[...props.columns, actionColumn]}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 10,
+            },
+          },
+        }}
+        slots={{ toolbar: GridToolbar }}
+        slotProps={{
+          toolbar: {
+            showQuickFilter: true,
+            quickFilterProps: { debounceMs: 500 },
+          },
+        }}
+        pageSizeOptions={[5]}
+        checkboxSelection
+        disableRowSelectionOnClick
+        disableColumnFilter
+        disableDensitySelector
+        disableColumnSelector
+      />
+    </div>
+  );
+};
+
+export default DataTable;
--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,10 +1,73 @@
-import React from 'react'
-import "./users.scss"
-
+import React from "react";
+import "./users.scss";
+import DataTable from "../../components/dataTable/DataTable";
+import {
+  DataGrid,
+  GridColDef,
+  GridValueGetterParams,
+  GridToolbar,
+} from "@mui/x-data-grid";
+import { userRows } from "../../data";
+import Add from "../../components/add/Add";
 const Users = () => {
+  const columns: GridColDef[] = [
+    { field: "id", headerName: "ID", width: 90 },
+    {
+      field: "img",
+      headerName: "Avatar",
+      width: 100,
+      renderCell: (params) => {
+        return <img src={params.row.img || "/noavatar.png"} alt="" />;
+      },
+    },
+    {
+      field: "firstName",
+      type: "string",
+      headerName: "First name",
+      width: 150,
+    },
+    {
+      field: "lastName",
+      type: "string",
+      headerName: "Last name",
+      width: 150,
+    },
+    {
+      field: "email",
+      type: "string",
+      headerName: "Email",
+      width: 200,
+    },
+    {
+      field: "phone",
+      type: "string",
+      headerName: "Phone",
+      width: 200,
+    },
+    {
+      field: "createdAt",
+      headerName: "Created At",
+      width: 200,
+      type: "string",
+    },
+    {
+      field: "verified",
+      headerName: "Verified",
+      width: 150,
+      type: "boolean",
+    },
+  ];
+  const [open, setOpen] = React.useState(false);
   return (
-    <div className='users'>Users</div>
-  )
-}
+    <div className="users">
+      <div className="info">
+        <h1>Users</h1>
+        <button onClick={() => setOpen(true)}>Add New User</button>
+      </div>
+      <DataTable slug="users" rows={userRows} columns={columns} />
+      {open && <Add setOpen={setOpen} slug="users" columns={columns} />}
+    </div>
+  );
+};
 
-export default Users
+export default Users;
